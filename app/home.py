181ad722fbd3a67a10715be710acb53f1143e2f0
abiraami279 <156501823+abiraami279@users.noreply.github.com>
2025-08-05 16:56:00 +0530

    Create home.py

---
+++ app/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def app():
+    st.title("🩺 Health Report Analyzer")
+    st.markdown("""
+    Welcome to the Health Report Analyzer – an AI-powered assistant for medical reports.
+
+    ### Features:
+    - 📄 Upload reports (PDF/image)
+    - 💬 Ask questions and get personalized answer
